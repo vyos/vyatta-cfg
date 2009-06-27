c3b23a060d2c949580873dc100469be9ad05734c
slioch <slioch@eng-140.vyatta.com>
2009-06-26 23:18:33 -0700

    updated perl api for new cli to support working node representation. not yet functional without additional testing.

--- src/commit2.c
+++ src/commit2.c
@@ -436,7 +436,13 @@ process_func(GNode *node, gpointer data)
 	status = execute_list(c->_def.actions[result->_action].vtw_list_head,&c->_def);
       }
       else {
-	fprintf(out_stream,"%s\t:\t%s\n",ActionNames[result->_action],d->_path);
+	char buf[MAX_LENGTH_DIR_PATH*sizeof(char)];
+	sprintf(buf,"%s\t:\t%s",ActionNames[result->_action],d->_path);
+	if (c->_def.multi) { //need to handle the embedded multinode as a special case--should be fixed!
+	  char *val = (char*)clind_unescape(d->_name);
+	  strcat(buf,val);
+	}
+	fprintf(out_stream,"%s\n",buf);
 	status = 1;
       }
       if (result->_action == delete_act) {
@@ -985,11 +991,19 @@ validate_func(GNode *node, gpointer data)
     char *buf = malloc(MAX_LENGTH_DIR_PATH*sizeof(char));
     if (IS_DELETE(d->_operation)) {
       sprintf(buf,"- %s",d->_path);
+      if (c->_def.multi) { //need to handle the embedded multinode as a special case--should be fixed!
+	char *val = (char*)clind_unescape(d->_name);
+	strcat(buf,val);
+      }
       coll = g_slist_append(coll,buf);
       result->_data = (void*)&coll;
     }
     else if (IS_SET_OR_CREATE(d->_operation)) {
       sprintf(buf,"+ %s",d->_path);
+      if (c->_def.multi) { //need to handle the embedded multinode as a special case--should be fixed!
+	char *val = (char*)clind_unescape(d->_name);
+	strcat(buf,val);
+      }
       coll = g_slist_append(coll,buf);
       result->_data = (void*)&coll;
     }
@@ -1052,7 +1066,13 @@ validate_func(GNode *node, gpointer data)
     status = execute_list(c->_def.actions[result->_action].vtw_list_head,&c->_def);
   }
   else {
-    fprintf(out_stream,"%s\t:\t%s\n",ActionNames[result->_action],d->_path);
+    char buf[MAX_LENGTH_DIR_PATH*sizeof(char)];
+    sprintf(buf,"%s\t:\t%s",ActionNames[result->_action],d->_path);
+    if (c->_def.multi) { //need to handle the embedded multinode as a special case--should be fixed!
+      char *val = (char*)clind_unescape(d->_name);
+      strcat(buf,val);
+    }
+    fprintf(out_stream,"%s\n",buf);
     status = 1;
   }
 
