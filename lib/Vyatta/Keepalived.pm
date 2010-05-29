vyos/vyatta-cfg

commit b68ac2b1969060ea6fa2f011a3107c83ad26e96d

Use opendir/grep to get rid of running ls/grep
Do file scanning in perl directly rather than using extra
programs. Motivated by desire to get rid of perlcritic warning
rather than any real performance issue.

lib/Vyatta/Keepalived.pm
@@ -164,17 +164,18 @@ sub intf_sort {
 sub get_state_files {
     my ($intf, $group) = @_;
 
+    opendir my $sdir, $state_dir
+	or die "Can't open $state_dir: $!\n";
+
     my @state_files;
-    my $LS;
     if ($group eq "all") {
-	open($LS,"ls $state_dir |grep '^vrrpd_$intf.*\.state\$' |");
+	@state_files = grep { /^vrrpd_$intf.*\.state$/ } $sdir;
     } else {
 	my $intf_group = $intf . "_" . $group . ".state";
-	open($LS,
-	     "ls $state_dir |grep '^vrrpd_$intf_group\$' |");
+	@state_files = grep { /^vrrpd_$intf_group$/ } $sdir;
     }
-    @state_files = <$LS>;
-    close($LS);
+    close $sdir;
+
     @state_files = intf_sort(@state_files);
     foreach my $i (0 .. $#state_files) {
 	$state_files[$i] = "$state_dir/$state_files[$i]";

commit d6d0e6acf15592a0547bbed90bd16ba8b813f0ed

Fix "show vrrp" regression caused by b68ac2b1969060ea6fa2f011a3107c83ad26e96d.

lib/Vyatta/Keepalived.pm
@@ -169,10 +169,10 @@ sub get_state_files {
 
     my @state_files;
     if ($group eq "all") {
-	@state_files = grep { /^vrrpd_$intf.*\.state$/ } $sdir;
+	@state_files = grep { /^vrrpd_$intf.*\.state$/ } readdir($sdir);
     } else {
 	my $intf_group = $intf . "_" . $group . ".state";
-	@state_files = grep { /^vrrpd_$intf_group$/ } $sdir;
+	@state_files = grep { /^vrrpd_$intf_group$/ } readdir($sdir);
     }
     close $sdir;
 