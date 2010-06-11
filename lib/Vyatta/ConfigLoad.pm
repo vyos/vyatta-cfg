50c72a3a96fca073dea1906e5135cf3e852fe0db
Michael Larson <slioch@slioch.vyatta.com>
2010-06-11 15:46:51 -0700

    Revert "allow for reverse difference between configurations."
    Not the right mod to support a reverse difference.
    
    This reverts commit ee5e4fb5ba06385bfd36c93eaa9cb7891c266c8e.

--- lib/Vyatta/ConfigLoad.pm
+++ lib/Vyatta/ConfigLoad.pm
@@ -384,7 +384,6 @@ sub findSetNodes {
 sub getConfigDiff {
   $active_cfg = new Vyatta::Config;
   $new_cfg_ref = shift;
-  my $reverse = shift;
   @set_list = ();
 #  @disable_list = ();
   @delete_list = ();
@@ -393,11 +392,6 @@ sub getConfigDiff {
 
   # need to filter out deletions of nodes with default values
   my @new_delete_list = ();
-  if ($reverse == 'true') {
-      my @tmp = @delete_list;
-      @delete_list = @set_list;
-      @set_list = @tmp;
-  }
   foreach my $del (@delete_list) {
     my @comps = map { 
 	my $file = $_;
