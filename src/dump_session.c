b550ff683e379086e2253d4aaaf11aafbc9fe6d7
James Davidson <james.davidson@vyatta.com>
2013-01-07 09:24:29 -0800

    Fix compiler warnings

--- src/dump_session.c
+++ src/dump_session.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <glib-2.0/glib.h>
+#include <glib-object.h>  /* g_type_init */
 #include "common/common.h"
 
 boolean g_debug = FALSE;
