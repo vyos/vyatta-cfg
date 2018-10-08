8d90ca824b53774b048a9570096d01fe22ce562e
UnicronNL <kim.sidney@gmail.com>
2018-10-09 00:48:42 +0200

    try to fix systemd boot issues

--- scripts/init/config-loader.sh
+++ scripts/init/config-loader.sh
@@ -9,10 +9,13 @@ GROUP=vyattacfg
 BOOTFILE=$vyatta_sysconfdir/config/config.boot
 
 if ! grep -q -w no-vyos-configure /proc/cmdline; then
-    log_progress_msg configure
     if [ -f /etc/default/vyatta-load-boot ]; then
         # build-specific environment for boot-time config loading
         source /etc/default/vyatta-load-boot
     fi
-    sg ${GROUP} -c "$vyatta_sbindir/vyatta-boot-config-loader $BOOTFILE"
+    sg ${GROUP} -c "$vyatta_sbindir/vyatta-boot-config-loader $BOOTFILE" &
 fi
+
+while ps -ef | grep my_commit | grep -v grep; do
+    sleep 1
+done
