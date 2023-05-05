a69fca2310d398681f3de3fa6efd903d6f00f710
Daniil Baturin <daniil@baturin.org>
2023-05-05 22:23:33 +0100

    T671: remove unused executables

--- utils/misc.h
+++
@@ -1,4 +0,0 @@
-int re_match(const char *string, char *pattern);
-int isValidPortNumber (int port);
-int isValidPortRange (int start, int stop);
-int isValidPortName (const char *name, const char *proto);
